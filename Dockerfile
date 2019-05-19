36e3f217b257bcb6ad3480bd5542190afb3cc86d
Camilo Nieto <caenietoba@unal.edu.co>
2019-05-19 17:43:42 -0500

    updated

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,21 @@
-	
-FROM node
+FROM node:8
 
+RUN mkdir -p /usr/src/app
 
+# Create app directory
 WORKDIR /usr/src/app
 
-COPY package.json .
-RUN npm install
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY package*.json ./
+COPY ./src/sql/product.sql /docker-entrypoint-initdb.d
 
+RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=productio
+# Bundle app source
 COPY . .
 
 EXPOSE 8080
-
-	
 CMD [ "npm", "start" ]
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql')
 
 module.exports = () => {
-    const connection =  mysql.createConnection({
-        host: 'localhost',
+    const connection =  mysql.createConnection({    
         user: 'root',
         password: 'f8cad56fa9',
-        database: 'product'
+        database: 'product',
+        host: '0.0.0.0',
+        //socketPath: '/var/run/mysqld/mysqld.sock'
     });
 
     connection.connect((err) => {
+        console.log(connection)
         if (err) {
             console.log('Error connecting to Database',err);
             return;
